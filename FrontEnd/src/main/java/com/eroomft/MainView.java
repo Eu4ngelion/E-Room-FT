ab97bcf2e00b36ba755f8e5172fe0e8d62000ea9
Saniyyah Intan <saniyyah585@gmail.com>
2025-05-29 13:38:56 +0800

    Simpan perubahan sebelum pull

--- FrontEnd/src/main/java/com/eroomft/MainView.java
+++ FrontEnd/src/main/java/com/eroomft/MainView.java
@@ -1,6 +1,8 @@
 package com.eroomft;
 
+import com.vaadin.flow.component.html.H4;
 import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
@@ -8,5 +10,10 @@ import com.vaadin.flow.router.Route;
 public class MainView extends VerticalLayout {
     public MainView() {
         add(new Span("Hello from Vaadin Frontend!"));
+HorizontalLayout horizontallayout = new HorizontalLayout();
+horizontallayout.getStyle().setFlexGrow("1");
+add(horizontallayout);
+H4 helloWorld = new H4("HELLO WORLD!!");
+add(helloWorld);
     }
 }
